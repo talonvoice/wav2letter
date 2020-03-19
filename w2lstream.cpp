b83ce04c4df651aefa499f4b19b302270529a19d
Ryan Hileman <lunixbochs@gmail.com>
2020-03-19 11:20:47 -0700

    support multiple streams

--- w2lstream.cpp
+++ w2lstream.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
 
@@ -106,34 +111,49 @@ public:
         dnnModule->add(featureModule);
         dnnModule->add(acousticModule);
         decoder = new GreedyCTC(tokens);
-
-        input = std::make_shared<streaming::ModuleProcessingState>(1);
-        output = dnnModule->start(input);
     }
 
     ~W2lStream() {
         delete decoder;
     }
 
-    std::string run(float *samples, size_t sample_count) {
+    std::string run(int64_t stream_id, float *samples, size_t sample_count) {
+        auto stream = streams.find(stream_id);
+        if (stream == streams.end()) {
+            auto tmpInput  = std::make_shared<streaming::ModuleProcessingState>(1);
+            auto tmpOutput = dnnModule->start(tmpInput);
+            streams[stream_id] = {std::move(tmpInput), std::move(tmpOutput)};
+            stream = streams.find(stream_id);
+        }
+        auto &input = stream->second.first;
+        auto &output = stream->second.second;
+
         auto inputBuffer = input->buffer(0);
         auto outputBuffer = output->buffer(0);
-
+        bool doReset = false;
         for (ssize_t i = 0; i < sample_count; i += chunk_size) {
-            ssize_t size = std::min((ssize_t)chunk_size, (ssize_t)sample_count);
+            ssize_t size = std::min((ssize_t)chunk_size, (ssize_t)sample_count - i);
             inputBuffer->ensure<float>(size);
             float *data = inputBuffer->data<float>();
             std::copy(samples + i, samples + i + size, data);
             inputBuffer->move<float>(size);
-            if (sample_count - i >= chunk_size) {
+            if (size == chunk_size) {
                 dnnModule->run(input);
             } else {
                 dnnModule->finish(input);
+                doReset = true;
             }
             decoder->run(outputBuffer->data<float>(), outputBuffer->size<float>());
+            outputBuffer->consume<float>(outputBuffer->size<float>());
         }
-        outputBuffer->consume<float>(outputBuffer->size<float>());
+        if (doReset) {
+            reset(stream_id);
+        }
         return decoder->text();
+    }
+
+    void reset(int64_t stream_id) {
+        streams.erase(stream_id);
     }
 
 public:
@@ -142,7 +162,10 @@ public:
 private:
     int chunk_size;
     std::shared_ptr<streaming::Sequential> dnnModule;
-    std::shared_ptr<streaming::ModuleProcessingState> input, output;
+    std::unordered_map<int64_t,
+        std::pair<
+            std::shared_ptr<streaming::ModuleProcessingState>,
+            std::shared_ptr<streaming::ModuleProcessingState> > > streams;
 
     std::vector<std::string> tokens;
     GreedyCTC *decoder;
@@ -158,10 +181,15 @@ w2lstream *w2lstream_new(const char *feature_model_path, const char *acoustic_mo
     return reinterpret_cast<w2lstream *>(stream);
 }
 
-char *w2lstream_run(w2lstream *engine, float *samples, size_t sample_count) {
+char *w2lstream_run(w2lstream *engine, int64_t stream_id, float *samples, size_t sample_count) {
     auto stream = reinterpret_cast<W2lStream *>(engine);
-    auto text = stream->run(samples, sample_count);
+    auto text = stream->run(stream_id, samples, sample_count);
     return strdup(text.c_str());
+}
+
+void w2lstream_reset(w2lstream *engine, int64_t stream_id) {
+    auto stream = reinterpret_cast<W2lStream *>(engine);
+    stream->reset(stream_id);
 }
 
 void w2lstream_free(w2lstream *engine) {
