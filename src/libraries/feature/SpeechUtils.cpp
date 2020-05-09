f6e6d6ccf70c31b18ec6376fd643f151e45d4ca3
Ryan Hileman <lunixbochs@gmail.com>
2020-05-09 14:44:41 -0700

    don't use fbgemm for libw2l if we have a better option
    such as (mkl or accelerate)

--- src/libraries/feature/SpeechUtils.cpp
+++ src/libraries/feature/SpeechUtils.cpp
@@ -13,8 +13,15 @@
 #include <chrono>
 #include <iostream>
 
+#if W2L_LIBRARIES_USE_MKL
+#include <mkl_cblas.h>
+#elif __APPLE__
+#include <Accelerate/Accelerate.h>
+#else
+#define FBGEMM_CBLAS_GEMM 1
 #include <fbgemm/FbgemmFP16.h>
 #include <omp.h>
+#endif
 
 namespace w2l {
 
@@ -36,6 +43,7 @@ std::vector<float> frameSignal(
   return frames;
 }
 
+#ifdef FBGEMM_CBLAS_GEMM
 std::vector<float> cblasGemm(
     const std::vector<float>& matA,
     const std::vector<float>& matB,
@@ -60,4 +68,38 @@ std::vector<float> cblasGemm(
 
   return matC;
 };
+#else // FBGEMM_CBLAS_GEMM
+
+std::vector<float> cblasGemm(
+    const std::vector<float>& matA,
+    const std::vector<float>& matB,
+    int n,
+    int k) {
+  if (n <= 0 || k <= 0 || matA.empty() || (matA.size() % k != 0) ||
+      (matB.size() != n * k)) {
+    throw std::invalid_argument("cblasGemm: invalid arguments");
+  }
+  int m = matA.size() / k;
+
+  std::vector<float> matC(m * n);
+  cblas_sgemm(
+      CblasRowMajor,
+      CblasNoTrans,
+      CblasNoTrans,
+      m,
+      n,
+      k,
+      1.0, // alpha
+      matA.data(),
+      k,
+      matB.data(),
+      n,
+      0.0, // beta
+      matC.data(),
+      n);
+  return matC;
+};
+
+#endif // FBGEMM_CBLAS_GEMM
+
 } // namespace w2l
