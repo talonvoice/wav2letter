c8acfb6062f3c4de55d6b83a0554b0797bd5b083
Ryan Hileman <lunixbochs@gmail.com>
2020-12-08 18:16:16 -0800

    pack w2l_emission struct

--- w2l/w2l_common.h
+++ w2l/w2l_common.h
@@ -4,9 +4,9 @@ extern "C" {
 #endif
 
 // malloc(sizeof(w2l_emission) + sizeof(float) * n_nrames * n_tokens);
-typedef struct w2l_emission {
-    int n_frames;
-    int n_tokens;
+typedef struct __attribute__((packed)) w2l_emission {
+    int32_t n_frames;
+    int32_t n_tokens;
     float matrix[0];
 } w2l_emission;
 
